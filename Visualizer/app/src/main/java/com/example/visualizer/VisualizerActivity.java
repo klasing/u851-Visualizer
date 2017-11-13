da701ef10d3e3f44db429b80eb171e7970b916c2
klasing <klasing1959@gmail.com>
2017-11-13 20:51:26 +0100

    13-11-2017 20:51 T06-01 Setup file, all empty

--- Visualizer/app/src/main/java/com/example/visualizer/VisualizerActivity.java
+++ Visualizer/app/src/main/java/com/example/visualizer/VisualizerActivity.java
@@ -1,5 +1,11 @@
 package com.example.visualizer;
-
+/*
+ * File -> Settings
+ * Editor -> File and Code templates
+ * select Class from list
+ * enter following text in right window
+ * #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end #parse("File Header.java") public class ${NAME} { }
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
